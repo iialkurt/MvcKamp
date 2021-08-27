8ff1edd88dc8f250cfc86bf96f3419478ea552d5
ismail alkurt <ialkurt@hotmail.com>
2021-08-27 12:39:21 +0300

    1. sertifika

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccesLayer.Concrete.Repositories;
+using EntityLayer.Congrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace BusinessLayer.Concrete
 {
     public class CategoryManager
     {
+        GenericReposirtory<Category> repo = new GenericReposirtory<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+
+        }
+
+        public void CatecoryAddBL(Category p)
+        {
+            if (p.CategoryName=="" || p.CategoryName.Length<=4 || p.CategoryDescription=="" | p.CategoryName.Length>=51)
+            {
+               //hata mesajı
+            }
+            else
+            {
+                repo.insert(p);
+            }
+        }
     }
 }
